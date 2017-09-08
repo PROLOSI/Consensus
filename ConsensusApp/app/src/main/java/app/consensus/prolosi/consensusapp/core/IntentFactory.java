4840e104426c5d7257d3bf640e4fd1a9e9ce312c
Jeison Andres Cespedes Morales <jeisoncespedes@gmail.com>
2017-09-08 17:30:52 -0600

    Se crea inicios de android
    Se incorpora Boostrap Para android

---
+++ ConsensusApp/app/src/main/java/app/consensus/prolosi/consensusapp/core/IntentFactory.java
@@ -0,0 +1,5 @@
+package app.consensus.prolosi.consensusapp.core;
+
+public class IntentFactory {
+    //TODO implement an Activity and Fragment delegate pattern
+}
---
+++ ConsensusApp/app/src/main/java/app/consensus/prolosi/consensusapp/core/ResumeTimerEvent.java
@@ -0,0 +1,7 @@
+package app.consensus.prolosi.consensusapp.core;
+
+/**
+ * Marker class for resuming a timer through Otto
+ */
+public class ResumeTimerEvent {
+}
