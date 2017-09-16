1833fdf5917e5e6641ee518402e117c600cd99a3
jeitae <jeisoncespedes@gmail.com>
2017-09-15 21:53:19 -0600

    Loggin y api

--- ConsensusApp/app/src/main/java/app/prolosi/consensus/consensusapp/MainActivity.java
+++ ConsensusApp/app/src/main/java/app/prolosi/consensus/consensusapp/MainActivity.java
@@ -1,13 +1,97 @@
 package app.prolosi.consensus.consensusapp;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import app.prolosi.consensus.consensusapp.Api.Autentication;
+import app.prolosi.consensus.consensusapp.Utilitario.Utility;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Autentication api;
+    boolean result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+
+        api = new Autentication();
+
+        result = Utility.checkPermission(this);
+
+        if (result) {
+
+            setContentView(R.layout.activity_main);
+
+        }
+
+
+          new autenticar().execute();
+
+
     }
+
+    private class autenticar extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            api.consumirApi();
+            return api.obtenerString("team_url");
+        }
+
+        @Override
+        protected void onPostExecute(String valor) {
+
+                Toast.makeText(MainActivity.this, valor, Toast.LENGTH_SHORT).show();
+
+        }
+
+    }
+
+
+    public void metEntrarSession (View view) {
+
+        Button button = (Button) findViewById(R.id.bIniciar_Loggin);
+        final EditText usuario = (EditText)findViewById(R.id.tCampoEmailUsuario_Loggin);
+        final EditText contrasenia = (EditText)findViewById(R.id.tCampoContrasenia_Loggin);
+        final TextView confirmar = (TextView)findViewById(R.id.tConfirmacionAPI);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                JSONObject parametros = new JSONObject();
+
+                try {
+                    parametros.put("usuario",String.valueOf(usuario.getText()));
+                    parametros.put("contrasenia",String.valueOf(contrasenia.getText()));
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                String respuesta = api.enviarDatos(parametros);
+
+                confirmar.setText("Confirmacion: "+respuesta+"");
+
+            }
+        });
+
+    }
+
 }
---
+++ ConsensusApp/app/src/main/java/app/prolosi/consensus/consensusapp/Utilitario/Utility.java
@@ -0,0 +1,99 @@
+package app.prolosi.consensus.consensusapp.Utilitario;
+import android.Manifest;
+import android.annotation.TargetApi;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+
+import app.prolosi.consensus.consensusapp.R;
+
+
+/**
+ * Created by Andres on 22/8/2017.
+ */
+
+public class Utility {
+    public static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 1;
+    public static final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 1;
+    public static final int MY_PERMISSIONS_REQUEST_INTERNET = 1;
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    public static boolean checkPermission(final Context context)
+    {
+        int currentAPIVersion = Build.VERSION.SDK_INT;
+        if(currentAPIVersion>=android.os.Build.VERSION_CODES.M)
+        {
+            if (ContextCompat.checkSelfPermission(context, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale((Activity) context, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
+                    alertBuilder.setCancelable(true);
+                    alertBuilder.setTitle(R.string.solicitarPermiso);
+                    alertBuilder.setMessage(R.string.permisoLeerMemoria);
+                    alertBuilder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                        }
+                    });
+                    AlertDialog alert = alertBuilder.create();
+                    alert.show();
+
+                } else {
+                    ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                }
+                return true;
+            } else if (ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale((Activity) context, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                    AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
+                    alertBuilder.setCancelable(true);
+                    alertBuilder.setTitle(R.string.solicitarPermiso);
+                    alertBuilder.setMessage(R.string.permisoEscribrirMemoria);
+                    alertBuilder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+                        }
+                    });
+                    AlertDialog alert = alertBuilder.create();
+                    alert.show();
+
+                } else {
+                    ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+                }
+                return true;
+            } else if (ContextCompat.checkSelfPermission(context, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale((Activity) context, Manifest.permission.INTERNET)) {
+                    AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
+                    alertBuilder.setCancelable(true);
+                    alertBuilder.setTitle(R.string.solicitarPermiso);
+                    alertBuilder.setMessage(R.string.permisoInternet);
+                    alertBuilder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.INTERNET}, MY_PERMISSIONS_REQUEST_INTERNET);
+                        }
+                    });
+                    AlertDialog alert = alertBuilder.create();
+                    alert.show();
+
+                } else {
+                    ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.INTERNET}, MY_PERMISSIONS_REQUEST_INTERNET);
+                }
+                return true;
+            } else {
+                return true;
+            }
+
+
+
+        } else {
+            return true;
+        }
+    }
+}
